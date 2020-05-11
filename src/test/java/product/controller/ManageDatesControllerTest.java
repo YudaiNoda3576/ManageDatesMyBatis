cc23221cea7497b443d85d6ca519216feb654641
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-11 09:47:53 +0900

    TestControllerの記述

--- src/test/java/product/controller/ManageDatesControllerTest.java
+++ src/test/java/product/controller/ManageDatesControllerTest.java
@@ -1,5 +1,21 @@
 package product.controller;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ManageDatesControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void 
+	
 }
